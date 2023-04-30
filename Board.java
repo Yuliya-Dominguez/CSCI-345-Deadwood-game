8f887226227afc38b96c48f6395f133443c4010e
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-04-29 17:46:14 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,3 +1,12 @@
 public class Board {
-    
-}
+    private int[] mapLayout;
+    private String playerPosition;
+
+    public void setMapLayout() {
+
+    }
+
+    public String showPlayerPos() {
+        return playerPosition;
+    }
+} 
