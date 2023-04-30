5e16c18433408caee1d675705bcd276e9affbe54
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-04-29 18:32:13 -0700

    Update LocationManager.java

--- LocationManager.java
+++ LocationManager.java
@@ -1,3 +1,16 @@
 public class LocationManager {
-    
+    public String[] playerLocations;
+    public String[] sceneNames;
+
+    public boolean validateMove() {
+        return true;
+    }
+
+    public void updateLocation() {
+
+    }
+
+    public void turnCards() {
+
+    }
 }
