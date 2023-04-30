490943d2b6d5c6b51d1cf537305ca29203311660
Yuliya-Dominguez <domingy@wwu.edu>
2023-04-29 17:01:34 -0700

    initial commit

---
+++ Deadwood.java
@@ -0,0 +1,16 @@
+public class Deadwood {
+
+    private String recordRole;
+    private int rehearseCounter;
+    private int score;
+    private int dayCount;
+    private String[] players;
+    private int[] gageStats;
+
+    public void gamePrep() {
+
+    }
+
+    
+
+}
