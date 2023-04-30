a651eeb97db4dde5cfcc8632bf74a82ac0c1b69e
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-04-29 17:14:21 -0700

    Update Day.java

--- Day.java
+++ Day.java
@@ -1,3 +1,17 @@
 public class Day {
-    
+    private int numOfPlayers;
+    private int dayCount;
+
+
+    public void endDay() {
+
+    }
+
+    public void finalDay() {
+
+    }
+
+    public void setUp() {
+
+    }
 }
