3b591cc8ac131940092e7118459b00fe11b81cbe
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-04-29 17:46:16 -0700

    Update Acting.java

--- Acting.java
+++ Acting.java
@@ -1,3 +1,21 @@
 public class Acting {
-    
+    private String playerRole;
+    private int shotsFinished;
+
+
+    public void successMainRole() {
+
+    }
+
+    public void successExtraRole() {
+        
+    }
+
+    public void failureMainRole() {
+        
+    }
+
+    public void failureExtraRole() {
+        
+    }
 }
