b3837c7a850199b35277c4e88007b74c68fc132d
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-04-29 18:07:40 -0700

    Update WrappingUp.java

--- WrappingUp.java
+++ WrappingUp.java
@@ -1,3 +1,18 @@
 public class WrappingUp {
-    
+    public String playerRole;
+    public boolean mainActorRoleFilled;
+    public int playerRank;
+
+    private boolean checkMainRoleFilled() {
+        return mainActorRoleFilled;
+    }
+
+    public void playerMainBonus() {
+
+    }
+
+    public void playerExtraBonus() {
+
+    }
+
 }
