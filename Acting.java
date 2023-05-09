2f1bac6fe036c6f5476c933d134258e291552a6c
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-05-08 19:40:45 -0700

    My work!

--- Acting.java
+++ Acting.java
@@ -2,20 +2,28 @@ public class Acting {
     public String playerRole;
     public int shotsFinished;
 
+    Player player = new Player();
+    Store store = new Store();
 
     public void successMainRole() {
-
+        System.out.println("Success!");
+        shotsFinished++;
+        store.payForShot();
     }
 
     public void successExtraRole() {
-        
+        System.out.println("Success!");
+        shotsFinished++;
+        store.payForShot();
     }
 
     public void failureMainRole() {
+        System.out.println("Failure.");
         
     }
 
     public void failureExtraRole() {
+        System.out.println("Failure.");
         
     }
 }
--- Dice.java
+++ Dice.java
@@ -2,17 +2,26 @@ import java.lang.Math;
 
 public class Dice {
 
-    private int playerDice;
     private int gameDice;
     private int[] bonusDice;
 
+    SceneCards sceneCards = new SceneCards();
 
-    public void readDice() {
+    public int readDice() {
+        gameDice = ((int) Math.random() * (6 - 1) + 1);
 
+        return gameDice;
     }
 
     public void readBonus() {
-        
+        int i = 0;
+        bonusDice = new int[sceneCards.cardRoleRank];
+        while (i < sceneCards.cardRoleRank) {
+            int roll = readDice();
+            bonusDice[i] = roll;
+            i++;
+        }
+
     }
 
 }
