30d84d1b885e7bef90577f6e84a34c5a70ab1a11
Yuliya-Dominguez <domingy@wwu.edu>
2023-05-13 00:13:13 -0700

    data structure work

---
+++ Parts.java
@@ -0,0 +1,45 @@
+public class Parts {
+    private String name;
+    private int level;
+    private Area area;
+    private String line;
+
+    public Parts(String name, int level, Area area, String line) {
+        this.name = name;
+        this.level = level;
+        this.area = area;
+        this.line = line;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+}
--- SceneCards.java
+++ SceneCards.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Document;
+
 public class SceneCards {
 
     public int cardRoleRank;
@@ -5,6 +10,90 @@ public class SceneCards {
     public int shotCounter;
     public String[] cardRoles;
     public String[] boardRoles;
+
+    private CardsXMLparser parser;//
+    private static ArrayList<Card> cardsList;//
+
+    public SceneCards() {
+        Document doc = null;
+        parser = new CardsXMLparser();
+        try {
+            doc = parser.getDocFromFile("cards.xml");
+            cardsList = parser.readCardsData(doc);
+        } catch (Exception e) {
+            System.out.println("Error = " + e);
+        }
+    }
+
+    public int getCardBudget(int index) {
+        Card card = cardsList.get(index);
+        int budget = card.getBudget();
+        System.out.println("budget = "+ budget);
+        return budget;
+    }
+
+    public static String getName(int index) {
+        Card card = cardsList.get(index);
+        String name = card.getCardName();
+        System.out.println("name = "+ name);
+        return name;
+     }
+
+    public int getSceneNumber(int index) {
+        Card card = cardsList.get(index);
+        Scene scene = card.getScene();
+        int number = scene.getNumber();
+        System.out.println("scene number = " + number);
+        return number;
+    }
+
+    public String getSceneDescription(int index) {
+        Card card = cardsList.get(index);
+        Scene scene = card.getScene();
+        String description = scene.getDescription();
+        System.out.println("scene description = " + description);
+        return description;
+    }
+
+    public static String getPartName(int cardIndex, int partIndex) {
+        Card card = cardsList.get(cardIndex);
+        List<Parts> partsList = card.getParts();
+        Parts part = partsList.get(partIndex);
+        String name = part.getName();
+        System.out.println("part name = " + name);
+        return name;
+     }
+     
+     public static int getPartLevel(int cardIndex, int partIndex) {
+        Card card = cardsList.get(cardIndex);
+        List<Parts> partsList = card.getParts();
+        Parts part = partsList.get(partIndex);
+        int level = part.getLevel();
+        System.out.println("part level = " + level);
+        return level;
+     }
+     
+     public static Area getPartArea(int cardIndex, int partIndex) {
+        Card card = cardsList.get(cardIndex);
+        List<Parts> partsList = card.getParts();
+        Parts part = partsList.get(partIndex);
+        Area area = part.getArea();
+        int x = area.getX();
+        int y = area.getY();
+        int h = area.getH();
+        int w = area.getW();
+        System.out.println("part area: x= " + x + ", y=" + y + ", h=" + h + ", w=" + w);
+        return area;
+     }
+     
+     public static String getPartLine(int cardIndex, int partIndex) {
+        Card card = cardsList.get(cardIndex);
+        List<Parts> partsList = card.getParts();
+        Parts part = partsList.get(partIndex);
+        String line = part.getLine();
+        System.out.println("part line = " + line);
+        return line;
+     }
 
     public void removeShot() {
 
