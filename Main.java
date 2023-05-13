0b769012760c0ad60726e7f35d03dedc810fd360
Silverson-EthanG <91219378+Silverson-EthanG@users.noreply.github.com>
2023-05-12 22:14:50 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    Player player = new Player();
     Deadwood deadwood = new Deadwood();
     Store store = Store.getStoreInstance();
     Board board = new Board();
@@ -54,17 +53,46 @@ public static void main(String[] args) {
             }
             System.out.println("Game is set up! \n"); 
         }
-
+        
         while (day.dayCount < DAY_MAX) {
-            System.out.println("Your turn! please type in your command, or 'options' for a list of available commands.\n");
-            String action = input.next();
+            for (Player player:gamePlayers) {
+                System.out.println("Your turn Player " + player.name + "! " + "Please type in your command, or 'options' for a list of available commands.\n");
+                String action = input.next();
 
-            if (action.equals("quit")) {
-                break;
+                if (action.equals("options")) {
+                    System.out.println("\n" + "'move [locationName]': Moves player to desired location, if possible.");
+                    System.out.println("'stats': Shows current player's stats and info.");
+                    System.out.println("'scenestats': Shows scene card's information that player is currently on/next to. ");
+                    System.out.println("'boardstats': Shows location player is on, including available board roles and neighboring locations.");
+                    System.out.println("'day': Shows current day in game.");
+                    System.out.println("'takerole [roleName]': Takes the role that you specified with [roleName]. Only works if your rank is equal to or higher than the role's rank.");
+                    System.out.println("'upgrade [rankLevel]': Upgrade your rank level (only works if you are at location 'Casting Office').");
+                    System.out.println("'act': Act your role (only works if you have taken a role in a scene).");
+                    System.out.println("'endturn': Ends current Player's turn. ");
+                    System.out.println("'quit': Quits the game.\n");
+                }
+
+                else if (action.equals("stats")) {
+                    player.checkInfo();
+                }
+
+                else if (action.equals("day")) {
+                    System.out.println("\n" +"This is day " + day.dayCount + "\n");
+                }
+
+                else if (action.equals("quit")) {
+                    System.out.println("Thank you! We are still working on the rest, so stay tuned!");
+                    System.exit(1);
+                }
+                else if (action.equals("endturn")) {
+                    System.out.println("Turn ended. \n");
+                    break;
+                }
+                else{
+                    System.out.println("\n" + "Sorry, that command is not available. Please type 'options' to see the list of available options. \n");
+                }
             }
         }   
-
-    System.out.println("Thank you! We are still working on the rest, so stay tuned!");
     input.close();
 }
 
